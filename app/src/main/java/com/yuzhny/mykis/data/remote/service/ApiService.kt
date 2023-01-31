4b554a431aaa8544b77ce4440a4ebef6276d3a52
nizhelya <nizhelskiy.sergey@gmail.com>
2023-01-31 14:50:10 +0200

    20230131-14:50

--- app/src/main/java/com/yuzhny/mykis/data/remote/service/ApiService.kt
+++ app/src/main/java/com/yuzhny/mykis/data/remote/service/ApiService.kt
@@ -11,11 +11,15 @@ import retrofit2.http.POST
 interface ApiService {
     companion object{
 //        const val SERVER_URL = "https://is.yuzhny.com/YkisMobileRest"
-        const val SERVER_URL = "http://195.138.91.67/YkisMobileRest"
+        const val SERVER_URL = "https://62.16.2.32/YkisMobileRest"
         const val BASE_URL = "$SERVER_URL/rest_api/"
-        const val GET_APPARTMENTS = "getAppartments.php"
+        const val GET_APPARTMENTS = "getAppartmentsById.php"
         const val DELETE_APPARTMENT = "deleteAppartment.php"
     }
+
+    //appartment
+
+    @FormUrlEncoded
     @POST(GET_APPARTMENTS)
     suspend fun getAppartments(): AppartmentEntity
 }
--- app/src/main/java/com/yuzhny/mykis/di/AppModule.kt
+++ app/src/main/java/com/yuzhny/mykis/di/AppModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Room
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.yuzhny.mykis.BuildConfig
 import com.yuzhny.mykis.data.cache.database.AppDatabase
 import com.yuzhny.mykis.data.dao.AppartmentDao
 import com.yuzhny.mykis.data.dao.FamilyDao
@@ -14,6 +15,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
@@ -24,20 +29,33 @@ import javax.inject.Singleton
 object AppModule {
     @Singleton
     @Provides
-    fun provideMoshi():Moshi{
+    fun provideMoshi(): Moshi {
         return Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
     }
+
     @Singleton
     @Provides
-    fun provideRetrofit(moshi :Moshi):Retrofit{
+    fun provideRetrofit(moshi: Moshi): Retrofit {
         return Retrofit.Builder()
             .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(createClient())
             .baseUrl(BASE_URL)
             .build()
 
     }
+
+    private fun createClient(): OkHttpClient {
+        val okHttpClientBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+            okHttpClientBuilder.addInterceptor(loggingInterceptor)
+        }
+        return okHttpClientBuilder.build()
+    }
+
     @Singleton
     @Provides
     fun provideAppDatabase(@ApplicationContext appContext: Context): AppDatabase {
@@ -50,14 +68,17 @@ object AppModule {
             .allowMainThreadQueries()
             .build()
     }
+
     @Provides
-    fun provideApiService(retrofit:Retrofit): ApiService {
+    fun provideApiService(retrofit: Retrofit): ApiService {
         return retrofit.create(ApiService::class.java)
     }
+
     @Provides
     fun provideAppartmentDao(db: AppDatabase): AppartmentDao {
         return db.appartmentDao()
     }
+
     @Provides
     fun provideFamilyDao(db: AppDatabase): FamilyDao {
         return db.familyDao()
